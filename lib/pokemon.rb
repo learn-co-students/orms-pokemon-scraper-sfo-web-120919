6280b90ed2eb4ef164425bba8c3000ab19b56853
github username <github email address>
2019-12-19 14:15:18 -0800

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -18,6 +18,6 @@ class Pokemon
     name = pokemon[1]
     type = pokemon[2]
 
-    new_pokemon = Pokemon.new(id:id, name:name, type:type, db:db)
+    Pokemon.new(id:id, name:name, type:type, db:db)
   end
 end
